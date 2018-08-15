4aaf901024299b5888d7b2eb67b76b086b401827
Michael DK Fowler <MichaelDKFowler@gmail.com>
2018-08-15 17:35:40 -0400

    First pass pipeline compatibility

---
+++ src/main/java/org/jenkinsci/plugins/ManagedBatchScript/ManagedBatchScript.java
@@ -0,0 +1,161 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jenkinsci.plugins.ManagedBatchScript;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.TaskListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jenkins.model.Jenkins;
+import org.jenkinsci.lib.configprovider.model.Config;
+import org.jenkinsci.plugins.configfiles.ConfigFiles;
+import org.jenkinsci.plugins.durabletask.DurableTaskDescriptor;
+import org.jenkinsci.plugins.durabletask.FileMonitoringTask;
+import org.jenkinsci.plugins.managedscripts.ManagedBatchScriptStep;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Runs a Windows batch script.
+ */
+public final class ManagedBatchScript extends FileMonitoringTask {
+
+    private static final Logger LOGGER = Logger.getLogger(ManagedBatchScriptStep.class.getName());
+    private final String script;
+    private boolean capturingOutput;
+    private final String[] buildStepArgs;
+
+    @DataBoundConstructor
+    public ManagedBatchScript(String script, String[] buildStepArgs) {
+        this.script = script;
+        this.buildStepArgs = buildStepArgs;
+    }
+
+    public String getScript() {
+        return script;
+    }
+
+    @Override
+    public void captureOutput() {
+        capturingOutput = true;
+    }
+
+    @SuppressFBWarnings(value = "VA_FORMAT_STRING_USES_NEWLINE", justification = "%n from master might be \\n")
+    @Override
+    protected FileMonitoringController doLaunch(FilePath ws, Launcher launcher, TaskListener listener, EnvVars envVars) throws IOException, InterruptedException {
+        if (launcher.isUnix()) {
+            throw new IOException("Batch scripts can only be run on Windows nodes");
+        }
+        BatchController c = new BatchController(ws);
+
+        String cmd;
+        if (capturingOutput) {
+            cmd = String.format("@echo off \r\ncmd /c \"\"%s\"\" > \"%s\" 2> \"%s\"\r\necho %%ERRORLEVEL%% > \"%s\"\r\n",
+                    quote(c.getBatchFile2(ws)),
+                    quote(c.getOutputFile(ws)),
+                    quote(c.getLogFile(ws)),
+                    quote(c.getResultFile(ws)));
+        } else {
+            cmd = String.format("@echo off \r\ncmd /c \"\"%s\"\" > \"%s\" 2>&1\r\necho %%ERRORLEVEL%% > \"%s\"\r\n",
+                    quote(c.getBatchFile2(ws)),
+                    quote(c.getLogFile(ws)),
+                    quote(c.getResultFile(ws)));
+        }
+        c.getBatchFile1(ws).write(cmd, "UTF-8");
+        c.getBatchFile2(ws).write(getMangedScriptCommandLine(ws), "UTF-8");
+
+        Launcher.ProcStarter ps = launcher.launch().cmds("cmd", "/c", "\"\"" + c.getBatchFile1(ws) + "\"\"").envs(escape(envVars)).pwd(ws).quiet(true);
+        listener.getLogger().println("[" + ws.getRemote().replaceFirst("^.+\\\\", "") + "] Running batch script"); // details printed by cmd
+        ps.readStdout().readStderr(); // TODO see BourneShellScript
+        ps.start();
+        return c;
+    }
+
+    private String getMangedScriptCommandLine(FilePath ws) {
+        String modifiedScript;
+        Config buildStepConfig = ConfigFiles.getByIdOrNull(Jenkins.getInstance(), script);
+        if (buildStepConfig == null) {
+            throw new IllegalStateException(org.jenkinsci.plugins.managedscripts.Messages.config_does_not_exist(script));
+        }
+        modifiedScript = buildStepConfig.content + "\r\nexit %ERRORLEVEL%";
+        try {
+            FilePath scriptPath = ws.createTextTempFile("managedbatch", ".bat", modifiedScript);
+            return buildCommandLine(scriptPath);
+        } catch (IOException | InterruptedException ex) {
+            LOGGER.log(Level.SEVERE, "Error creating tmp file");
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private String buildCommandLine(FilePath script) {
+        StringBuilder commandline = new StringBuilder("cmd /c call ");
+        commandline.append(script.getRemote());
+
+        // Add additional parameters set by user
+        if (buildStepArgs != null) {
+            for (String arg : buildStepArgs) {
+                 commandline.append(" ");
+                commandline.append(arg);
+            }
+        }
+
+        return commandline.toString();
+    }
+
+    private static String quote(FilePath f) {
+        return f.getRemote().replace("%", "%%");
+    }
+
+    private static final class BatchController extends FileMonitoringController {
+
+        private BatchController(FilePath ws) throws IOException, InterruptedException {
+            super(ws);
+        }
+
+        public FilePath getBatchFile1(FilePath ws) throws IOException, InterruptedException {
+            return controlDir(ws).child("jenkins-wrap.bat");
+        }
+
+        public FilePath getBatchFile2(FilePath ws) throws IOException, InterruptedException {
+            return controlDir(ws).child("jenkins-main.bat");
+        }
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends DurableTaskDescriptor {
+
+        @Override
+        public String getDisplayName() {
+            return "Managed Windows batch";
+        }
+
+    }
+
+}
---
+++ src/main/java/org/jenkinsci/plugins/managedscripts/ManagedBatchScriptStep.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jenkinsci.plugins.managedscripts;
+import hudson.Extension;
+import hudson.model.ItemGroup;
+import hudson.util.ListBoxModel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.logging.Logger;
+import org.jenkinsci.Symbol;
+import org.jenkinsci.lib.configprovider.model.Config;
+import org.jenkinsci.plugins.ManagedBatchScript.ManagedBatchScript;
+import org.jenkinsci.plugins.configfiles.ConfigFiles;
+import org.jenkinsci.plugins.durabletask.DurableTask;
+import org.jenkinsci.plugins.workflow.steps.durable_task.DurableTaskStep;
+import org.kohsuke.stapler.AncestorInPath;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+
+/**
+ *
+ * @author MFowler
+ */
+public class ManagedBatchScriptStep extends DurableTaskStep{
+    private final String scriptId;
+    private String[] buildStepArgs;
+    private ScriptBuildStepArgs scriptBuildStepArgs;
+    private static final Logger LOGGER = Logger.getLogger(ManagedBatchScriptStep.class.getName());
+    
+    public static class ArgValue {
+        public final String arg;
+
+        @DataBoundConstructor
+        public ArgValue(String arg) {
+            this.arg = arg;
+        }
+    }
+
+    public static class ScriptBuildStepArgs {
+        public final boolean defineArgs;
+        public final ArgValue[] buildStepArgs;
+
+        @DataBoundConstructor
+        public ScriptBuildStepArgs(boolean defineArgs, ArgValue[] buildStepArgs) {
+            this.defineArgs = defineArgs;
+            this.buildStepArgs = buildStepArgs == null ? new ArgValue[0] : Arrays.copyOf(buildStepArgs, buildStepArgs.length);
+        }
+    }
+    
+    public String getBuildStepId() {
+        return scriptId;
+    }
+
+    public String[] getBuildStepArgs() {
+        String[] args = buildStepArgs == null ? new String[0] : buildStepArgs;
+        return Arrays.copyOf(args, args.length);
+    }
+    
+    public ScriptBuildStepArgs getScriptBuildStepArgs(){
+        return scriptBuildStepArgs;
+    }
+    
+    @DataBoundSetter
+    public void setScriptBuildStepArgs(ScriptBuildStepArgs scriptBuildStepArgs){
+       this.scriptBuildStepArgs= scriptBuildStepArgs;
+       List<String> l = null;
+        if (scriptBuildStepArgs != null && scriptBuildStepArgs.defineArgs
+                && scriptBuildStepArgs.buildStepArgs != null) {
+            l = new ArrayList<>();
+            for (ArgValue arg : scriptBuildStepArgs.buildStepArgs) {
+                l.add(arg.arg);
+            }
+        }
+        this.buildStepArgs = l == null ? null : l.toArray(new String[l.size()]);
+    }
+    
+    @DataBoundConstructor
+    public ManagedBatchScriptStep(String buildStepId) {
+        if (buildStepId == null) {
+            throw new IllegalArgumentException();
+        }
+        this.scriptId = buildStepId;
+    }
+    
+    @Override
+    protected DurableTask task() {
+        return new ManagedBatchScript(scriptId, buildStepArgs);
+    }
+    
+    @Symbol("managedbat")
+    @Extension
+    public static final class DescriptorImpl extends DurableTaskStepDescriptor {
+
+        @Override public String getDisplayName() {
+            return "Managed Windows Batch Script";
+        }
+
+        @Override public String getFunctionName() {
+            return "managedbat";
+        }
+        
+        public ListBoxModel doFillBuildStepIdItems(@AncestorInPath ItemGroup context) {
+            List<Config> configsInContext = ConfigFiles.getConfigsInContext(context, WinBatchConfig.WinBatchConfigProvider.class);
+            Collections.sort(configsInContext, new Comparator<Config>() {
+                @Override
+                public int compare(Config o1, Config o2) {
+                    return o1.name.compareTo(o2.name);
+                }
+            });
+            ListBoxModel items = new ListBoxModel();
+            items.add("please select", "");
+            for (Config config : configsInContext) {
+                items.add(config.name, config.id);
+            }
+            return items;
+        }
+
+    }
+}
